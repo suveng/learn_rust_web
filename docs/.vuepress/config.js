72928ea72a01ac2440a586bed4cd999ecae0c739
suwenguang <wenguang.su@ly.com>
2024-01-04 12:19:56 +0800

    文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,35 +4,38 @@ export default defineUserConfig({
     // 将 base 设置为 "/<REPO>/"
     base : '/learn_rust_web/',
     lang: 'zh-CN',
-    title: 'rust Web 的 渐进式教程',
-    description: 'rust Web 的 渐进式教程',
+    title: 'RustWeb的渐进式教程',
+    description: 'RustWeb的渐进式教程',
     theme: defaultTheme({
         // 在这里进行配置
         navbar: [
             // NavbarItem
-            {
-                text: 'RustWeb的渐进式教程',
-                link: '/rust/',
-            },
+            // {
+            //     text: 'RustWeb的渐进式教程',
+            //     link: '/rust/',
+            // },
             // NavbarGroup
             {
-                text: '联系方式',
-                children: [
-                    'https://baidu.com',
-                ],
+                text: '寻求rust开发工作',
+                link: '/resume/resume.md',
             },
         ],
         // 侧边栏对象
         // 不同子路径下的页面会使用不同的侧边栏
         sidebar: {
-            '/rust/': [
+            '/': [
                 {
                     text : 'RustWeb的渐进式教程',
                     children : [
                         {
                             text : '1_初始化项目.md',
                             link : '/rust/1_初始化项目.md'
-                        }
+                        },
+                        {
+                            text : '2_编写简单的grpc接口.md',
+                            link : '/rust/2_编写简单的grpc接口.md'
+                        },
+
                     ]
                 }
             ]
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,21 @@
-hello vuepress 
+# RustWeb渐进式教程
 
-测试github deploy action
+github: [https://github.com/suveng/learn_rust_web](https://github.com/suveng/learn_rust_web)
+
+## 目的
+
+记录 Rust Web 开发的过程, 分享经验, 帮助大家更好的学习 Rust Web 开发.
+
+## 项目实现效果
+
+todo!
+
+## 技术栈
+
+- Rust 
+- grpc + Tonic 
+
+
+## 求职
+
+[寻求一份Rust开发工作, 点击查看我的简历](resume/resume.md)
